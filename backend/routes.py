1955df1f3a34041846d83ec6e72c5c541daa2d94
tashigyatso <tashigyatso45@gmail.com>
2024-10-02 19:45:56 -0400

    add a post routing

--- backend/routes.py
+++ backend/routes.py
@@ -9,3 +9,30 @@ def get_friends():
   friends = Friend.query.all()
   result = [friend.to_json() for friend in friends ]
   return jsonify(result), 200
+# create a friend 
+@app.route("/api/friends", methods=["POST"])
+def create_friend():
+   try:
+      data = request.json
+
+      name=data.get("name")
+      role=data.get("role")
+      description=data.get("description")
+      gender=data.get("gender")
+
+      if gender == "male":
+         img_url = f"https://avatar.iran.liara.run/public/boy?username={name}"
+      elif gender == "female":
+         img_url = f"https://avatar.iran.liara.run/public/girl?username={name}"
+      else:
+         img_url = None
+
+      new_friend = Friend(name=name, role=role, description=description, gender=gender, img_url=img_url)
+
+      db.session.add(new_friend)
+      db.session.commit()
+      return jsonify({"msg":"friend created successfully"}), 201 
+  
+   except Exception as e:
+      db.session.rollback()
+      return jsonify({"error":str(e)}), 500
