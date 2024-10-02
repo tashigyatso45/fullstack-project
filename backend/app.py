ce1806c33d7fcfe82d8ef1d29abce0523b50f93b
tashigyatso <tashigyatso45@gmail.com>
2024-10-01 21:18:51 -0400

    created routes and models

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
-from flask_cros import CORS
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite://friends.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///friends.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 
 db= SQLAlchemy(app)
+import routes
+with app.app_context():
+  db.create_all()
 
 app.run(debug=True)
---
+++ backend/routes.py
@@ -0,0 +1,11 @@
+from app import app, db 
+from flask import request, jsonify
+from models import Friend
+
+
+# get all friends
+@app.route("/api/friends", methods=["GET"])
+def get_friends():
+  friends = Friend.query.all()
+  result = [friend.to_json() for friend in friends ]
+  return jsonify(result), 200
